f71619d18902a528ae0976be90d04e6309178b7b
Blake <83431780+blakehoosline@users.noreply.github.com>
2023-02-22 13:25:12 -0600

    Create soc.h

---
+++ soc.h
@@ -0,0 +1,20 @@
+#ifndef SOC_H
+#define SOC_H
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+
+#define SERVERPORT 31254
+#define SERVERPORTSTR "31254"
+#define SERVERIP "199.17.28.80"
+#define SERVERNAME ""
+
+#endif
